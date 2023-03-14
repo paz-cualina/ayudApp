321db0f213527d32a07e955b133b65766e0890dd
Tamara Sancristobal <tamara.sancristobal@decemberlabs.com>
2023-03-14 11:54:49 -0300

    NavBar style changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Banner from '../src/Components/Banner/Banner';
 import FiltersBar from './Components/FiltersBar/FiltersBar';
 import ItemListContainer from './Pages/ItemListContainer';
 import ItemDetailsContainer from './Pages/ItemDetailsContainer';
 import NavBar from './Components/NavBar/NavBar';
-import logoAyudapp from './assets/img/logoAyudapp.png';
 import CartProvider from './context/CartProvider';
 import Cart from './Pages/Cart';
 import './App.css';
@@ -12,34 +11,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className='topWrapper'>
-      <section className="content">
+    <div className='mainWrapper'>
 
         <BrowserRouter>
           <CartProvider>
 
-            <Link to='/'>
-              <img src={logoAyudapp} className="logo" />
-            </Link>
-          
             <NavBar />
-            <Banner />
-            <FiltersBar />
-
-            <Routes>
-
-              <Route path='/' element={<ItemListContainer />} />
-              <Route path='/item/:id' element={<ItemDetailsContainer />} />
-              <Route path='/category/:categoryId' element={<ItemListContainer />} />
-              <Route path='/cart' element={<Cart />} />
-              <Route path='*' element={<div><h1>Esta página no existe</h1></div>} />
-
-            </Routes>
             
+            <section className="content">
+              <Banner />
+              <FiltersBar />
+
+              <Routes>
+
+                <Route path='/' element={<ItemListContainer />} />
+                <Route path='/item/:id' element={<ItemDetailsContainer />} />
+                <Route path='/category/:categoryId' element={<ItemListContainer />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='*' element={<div><h1>Esta página no existe</h1></div>} />
+
+              </Routes>
+            </section>
+
           </CartProvider>
         </BrowserRouter>
 
-      </section>
     </div>
   )
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,33 +1,25 @@
 import CartWidget from '../CartWidget/CartWidget.js';
-import heart from './../../assets/img/heart.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import logoAyudapp from '../../assets/img/logoAyudapp.png';
 
 export default function NavBar() {
     return (
         <div className="navBar">
-            <div className="wrapper">
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li><NavLink to="/">Nosotros</NavLink></li>
-                    <li className="categories">
-                        <NavLink to="/">Colaboraciones</NavLink>
-                        <ul>
-                            <li> <NavLink to='/category/cirugias'>Cirugías</NavLink></li>
-                            <li><NavLink to='/category/tratamientos'>Tratamientos</NavLink></li>
-                            <li><NavLink to="/category/alimentacion">Alimentación</NavLink></li>
-                        </ul>
-                    </li>
-                </ul>
-                <div className="right">
-                    <NavLink to="/">
-                        <img src={heart} />
-                        Lista de espera
-                    </NavLink>
-                    <CartWidget />
-                </div>
-            </div>
+            <Link to='/'>
+                <img src={logoAyudapp} className="logo" />
+            </Link>
+            <ul>
+                <li><NavLink to="/">Nosotros</NavLink></li>
+                <li className="categories">
+                    <NavLink to="/">Colaboraciones</NavLink>
+                    <ul>
+                        <li> <NavLink to='/category/cirugias'>Cirugías</NavLink></li>
+                        <li><NavLink to='/category/tratamientos'>Tratamientos</NavLink></li>
+                        <li><NavLink to="/category/alimentacion">Alimentación</NavLink></li>
+                    </ul>
+                </li>
+                <li><CartWidget /></li>
+            </ul>
         </div>
     )
 }
