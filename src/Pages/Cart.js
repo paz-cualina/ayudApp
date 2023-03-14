cff34268aa4a0345d5404c21426c55c1f2186c8e
Tamara Sancristobal <tamara.sancristobal@decemberlabs.com>
2023-03-14 00:41:34 -0300

    Favicon and cart style

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,10 +1,40 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
+import catPlaceholder from "../assets/img/catPlaceholder.png";
+import { collection, getFirestore, addDoc } from "firebase/firestore";
 
 const Cart = () => {
     const {cart, clear, removeItem} = useContext(CartContext);
     const navigate = useNavigate();
+
+    const createOrder = (event) => {
+        event.preventDefault();
+        const db = getFirestore();
+        const querySnapshot = collection(db, 'orders');
+
+        addDoc(querySnapshot, {
+            buyer: {
+                email: 'diegovazquez@ayudapp.com',
+                name: 'Diego Vazquez',
+                phone: '91546798'
+            }, 
+            cases: cart.map((product) => {
+                return {
+                    title: product.title,
+                    cost: product.cost,
+                    id: product.id,
+                    quantity: product.quantity
+                }
+            }),
+            total: cart.reduce((acc, curr) => acc + curr.cost * curr.quantity, 0)
+        })
+        .then((response) => {
+            console.log(response.id)
+        })
+        .catch((error) => console.log(error))
+    };
+
     return (
         <div className="cartProduct">
             <h2>Resumen de Donaciones:</h2>
@@ -12,11 +42,18 @@ const Cart = () => {
                 <div key={product.name} className="singleProduct">
                     <h3>{product.name}</h3>
                     <h5>{product.quantity}</h5>
+                    <figure>
+                        <img src={catPlaceholder} alt={product.name} />
+                    </figure>
                     <button onClick={() => removeItem(product.id)} className="delete">X</button>
                 </div>
             ))}
             {
-                cart.length > 0 && <button onClick={clear}>Vaciar Carrito</button>
+                cart.length > 0 && 
+                <>
+                    <button onClick={clear}>Vaciar Carrito</button>
+                    <button onClick={createOrder}>Completar donación</button>
+                </>
             }
             {
                 cart.length === 0 && 
--- src/Pages/ItemListContainer.js
+++ src/Pages/ItemListContainer.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ItemList from '../Components/ItemList/ItemList';
-import { getFirestore, getDocs, collection } from 'firebase/firestore';
+import { getFirestore, getDocs, collection, query, where } from 'firebase/firestore';
 
 const ItemListContainer = () => {
     const [productList, setProductList] = useState([]); //vacio porque lo rellena ejecucion de useEffect
@@ -11,17 +11,34 @@ const ItemListContainer = () => {
       const db = getFirestore();
       const querySnapshot = collection(db, 'items');
 
-      getDocs(querySnapshot)
+      if (categoryId) {
+        const filteredQuery = query(querySnapshot, where('category', '==', categoryId)) 
+        getDocs(filteredQuery)
         .then((response) => {
           const list = response.docs.map((doc) => {
             return {
               id: doc.id, 
               ...doc.data()
+
             };
           });
+          console.log(list);
           setProductList(list);
         })
         .catch((error) => console.log(error));
+      } else {
+        getDocs(querySnapshot)
+          .then((response) => {
+            const list = response.docs.map((doc) => {
+              return {
+                id: doc.id, 
+                ...doc.data()
+              };
+            });
+            setProductList(list);
+          })
+          .catch((error) => console.log(error));
+      }
     }
 
     useEffect(() => { getProducts(); }, [categoryId]);
